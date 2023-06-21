54697493ae3e98fc228baacbda691e35ae1b54d3
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2023-06-21 23:34:50 +0000

    Dynamic Data using Templates
    we can send any data to html file , just put it under {{}} double curly brackets

--- app.py
+++ app.py
@@ -3,9 +3,38 @@ from flask import render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id":1,
+    "title":"Data Analyst",
+    "location":"Bengaluru, India",
+    "salary":"Rs. 10,00,00"
+  },
+  {
+    "id":2,
+    "title":"Data Scientist",
+    "location":"Delhi, India",
+    "salary":"Rs. 15,00,00"
+  },
+  {
+    "id":3,
+    "title":"Frontend Engineer",
+    "location":"Remote",
+    "salary":"Rs. 12,00,00"
+  },
+  {
+    "id":4,
+    "title":"Backend Engineer",
+    "location":"San Francisco, USA",
+    "salary":"$120,000"
+  }
+  
+  
+]
+
 @app.route("/")
 def hello_word():
-  return render_template("home.html")
+  return render_template("home.html",jobs_to_show = JOBS)
   
 
 
