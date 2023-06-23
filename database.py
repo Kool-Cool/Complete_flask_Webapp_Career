ca3f3e7f11e4c8d6bc0caa1badcf8b54dd4d54b9
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2023-06-23 04:50:07 +0000

    Connecting database securely
    connected security.
    # NOTE
    # MAKE sure you have saved data of Connection strings from PLanetScale

--- database.py
+++ database.py
@@ -1,11 +1,32 @@
 import sqlalchemy 
 from sqlalchemy import create_engine
-
+from sqlalchemy import text
 
 print(sqlalchemy.__version__)
+
+# NOTE
+# MAKE sure you have saved data of Connection strings from PLanetScale
+HOST="aws.connect.psdb.cloud"
+USERNAME="4eq8suy3m1jd67tcjux9"
+PASSWORD="pscale_pw_KrGuBvjmfwtZiDhYu0arCFCS75Stss382Zs5PntjdF8"
+DATABASE="joviancareers"
+
 
 
 # For establishing connections between mysql and sqlalchemy
 # engine = create_engine("mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4")
+db_connection_string = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?charset=utf8mb4"
 
-engine = create_engine("mysql+pymysql://2zrrxqiz2xmqw0xrig7z:pscale_pw_bcakfQWgKZBZvnbkBzjx9PMSXIU79pES8BYa1gpKsjA@aws.connect.psdb.cloud/dbname?charset=utf8mb4")
+engine = create_engine(db_connection_string ,
+                      connect_args={
+                        "ssl":{
+                          "ssl_ca":"/etc/ssl/cert.pem"
+                        }
+                      }
+                      )
+
+with engine.connect() as conn:
+  db_query = "select * from jobs"
+  result = conn.execute(text(db_query))
+
+  print(result.all())
