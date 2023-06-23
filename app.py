d7774dafc176d1fa7fddabb9ed47d49985676359
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2023-06-23 11:05:21 +0000

    Setting up new html file , jobpage.html

--- app.py
+++ app.py
@@ -29,7 +29,9 @@ def list_jobs():
 @app.route("/job/<id>") #Dynamic route # Kinda REST API (GOES THROUGH DATA AND RETURN JSON FILE)
 def show_job(id):
   job = load_job_from_db_of_id(id)
-  return jsonify(job)
+  if not job:
+    return "Not Found",404
+  return render_template("jobpage.html",job=job)
 
 
 if __name__ == "__main__":
